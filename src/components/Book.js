c632f5ec62e380f0babba1069737aa2da288cc0a
cdrainxv <cdrainxv@users.noreply.github.com>
2018-12-05 00:12:39 -0700

    Book component

---
+++ src/components/Book.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Book = ({ book: { id, title, category } }) => (
+  <tr>
+    <td>{id}</td>
+    <td>{title}</td>
+    <td>{category}</td>
+  </tr>
+)
+
+export default Book
