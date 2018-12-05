5fb53e1a6970a24486ff3c249689ccd3461add50
cdrainxv <cdrainxv@users.noreply.github.com>
2018-12-05 08:38:19 -0700

    conditional render of books based on filter

--- src/containers/BooksList.js
+++ src/containers/BooksList.js
@@ -3,7 +3,25 @@ import { connect } from 'react-redux'
 import { removeBook } from '../actions'
 import Book from '../components/Book'
 
-const BooksList = ({ books, removeBook }) => {
+const BooksList = ({ books, filter, removeBook }) => {
+  const isFilterable = filter !== 'All'
+
+  const renderBooks = () => {
+    if (isFilterable) {
+      const filteredBooksList = Object.values(books).filter(
+        book => book.category === filter
+      )
+
+      return filteredBooksList.map(book => (
+        <Book handleRemoveBook={removeBook} key={book.id} book={book} />
+      ))
+    } else {
+      return Object.values(books).map(book => (
+        <Book handleRemoveBook={removeBook} key={book.id} book={book} />
+      ))
+    }
+  }
+
   return (
     <table>
       <thead>
@@ -14,16 +32,12 @@ const BooksList = ({ books, removeBook }) => {
           <th>Remove Book</th>
         </tr>
       </thead>
-      <tbody>
-        {Object.values(books).map(book => (
-          <Book handleRemoveBook={removeBook} key={book.id} book={book} />
-        ))}
-      </tbody>
+      <tbody>{renderBooks()}</tbody>
     </table>
   )
 }
 
-const mapStateToProps = ({ books }) => ({ books })
+const mapStateToProps = ({ books, filter }) => ({ books, filter })
 
 export default connect(
   mapStateToProps,
