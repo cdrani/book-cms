f39a60002baf8244d3e2e05e3942a50ce90e1dc9
cdrainxv <cdrainxv@users.noreply.github.com>
2018-12-05 00:00:07 -0700

    implement App component

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import BooksList from './BooksList'
+import BooksForm from './BooksForm'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <BooksList />
+        <BooksForm />
+      </div>
+    )
+  }
+}
+
+export default App
