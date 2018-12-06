afff681784c188333894042ed5744cf9c7ac9a32
cdrainxv <cdrainxv@users.noreply.github.com>
2018-12-05 22:33:40 -0700

    style CategoryFilter component

--- src/containers/CategoryFilter.js
+++ src/containers/CategoryFilter.js
@@ -1,7 +1,20 @@
 import React from 'react'
+import styled from 'styled-components'
 import { connect } from 'react-redux'
 import { categories } from '../constants'
 import { setFilter } from '../actions'
+
+
+const Select = styled.select`
+  width: 52%;
+  height: 40px;
+  font-size: 1rem;
+  border: solid 1px #e8e8e8;
+  background-color: #fff;
+  opacity: 0.85;
+  outline: 0;
+  margin-bottom: 20px;
+`
 
 const CategoryFilter = ({ filter, setFilter }) => {
   const handleSetFilter = e => {
@@ -10,11 +23,11 @@ const CategoryFilter = ({ filter, setFilter }) => {
   }
 
   return (
-    <select value={filter} onChange={handleSetFilter}>
+    <Select value={filter} onChange={handleSetFilter}>
       {['All', ...categories].map(category => (
         <option key={category}>{category}</option>
       ))}
-    </select>
+    </Select>
   )
 }
 
