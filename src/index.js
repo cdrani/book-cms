763ad2f16505a7e0374f742daaeff4e33ed451bb
cdrainxv <cdrainxv@users.noreply.github.com>
2018-12-13 12:08:19 -0700

    setup context to get token

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import { ApolloProvider } from 'react-apollo'
 import { ApolloClient } from 'apollo-client'
 import { HttpLink } from 'apollo-link-http'
 import { InMemoryCache } from 'apollo-cache-inmemory'
+import { setContext } from 'apollo-link-context'
 
 import { store } from './store'
 import './index.css'
@@ -17,9 +18,20 @@ const httpLink = new HttpLink({
   uri: BOOKCMS_API
 })
 
-const cache = new InMemoryCache()
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('token')
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : ''
+    }
+  }
+})
 
-const client = new ApolloClient({ link: httpLink, cache })
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache()
+})
 
 ReactDOM.render(
   <ApolloProvider client={client}>
