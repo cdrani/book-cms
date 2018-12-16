2a425dbe28f1986925358556820bbbdaa73cf3a0
cdrainxv <cdrainxv@users.noreply.github.com>
2018-12-15 23:21:42 -0700

    implement pagination

--- src/containers/BooksList.js
+++ src/containers/BooksList.js
@@ -1,22 +1,58 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import { Query } from 'react-apollo'
 
 import Book from '../components/Book'
 import { MYBOOKS } from '../constants'
 
+const updateQuery = (previousResult, { fetchMoreResult }) => {
+  if (!fetchMoreResult) {
+    return previousResult
+  }
+
+  return {
+    ...previousResult,
+    myBooks: {
+      ...previousResult.myBooks,
+      ...fetchMoreResult.myBooks
+    },
+    edges: [...previousResult.myBooks.edges, ...fetchMoreResult.myBooks.edges]
+  }
+}
+
 const BooksList = () => {
   return (
     <Query query={MYBOOKS} variables={{ input: { limit: 5 } }}>
-      {({ loading, error, data, client }) => {
+      {({ loading, error, data, fetchMore }) => {
         if (loading) return 'loading'
         if (error) {
           return error.toString()
         }
 
         const {
-          myBooks: { edges: books }
+          myBooks: { edges: books, pageInfo }
         } = data
-        return books.map(book => <Book key={book.id} book={book} />)
+
+        return (
+          <Fragment>
+            {books.map(book => (
+              <Book key={book.id} book={book} pageInfo={pageInfo} />
+            ))}
+
+            {pageInfo.hasNextPage && (
+              <button
+                type="button"
+                onClick={() =>
+                  fetchMore({
+                    variables: { input: { cursor: pageInfo.endCursor } },
+                    updateQuery
+                  })
+                }
+              >
+                Fetch More Books
+              </button>
+            )}
+          </Fragment>
+        )
       }}
     </Query>
   )
