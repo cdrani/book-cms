642fc795ad3c06a3e53eb667d28ae8b2f745ecbc
cdrainxv <cdrainxv@users.noreply.github.com>
2019-01-31 19:22:54 -0700

    replace apollo Mutation with useMutation hook

--- src/containers/BooksForm.js
+++ src/containers/BooksForm.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import { Mutation, compose, graphql } from 'react-apollo'
+import { useMutation } from 'react-apollo-hooks'
 
 import {
   categories,
@@ -45,116 +46,119 @@ const BooksForm = ({ addToCategories, handleClose }) => {
     setChapters(e.target.value)
   }
 
-  const updateCache = (cache, { data: { createBook } }) => {
-    const {
-      myBooks: { edges, pageInfo }
-    } = cache.readQuery({
-      query: MYBOOKS,
-      variables: { input: { limit: 5 } }
-    })
+  const addBook = useMutation(CREATEBOOK, {
+    update: (cache, { data: { createBook } }) => {
+      const {
+        myBooks: { edges, pageInfo }
+      } = cache.readQuery({
+        query: MYBOOKS,
+        variables: { input: { limit: 5 } }
+      })
 
-    cache.writeQuery({
-      query: MYBOOKS,
-      variables: { input: { limit: 5 } },
-      data: {
-        myBooks: {
-          __typename: 'BookConnection',
-          edges: [Object.assign(createBook, { __typename: 'Book' }), ...edges],
-          pageInfo
+      cache.writeQuery({
+        query: MYBOOKS,
+        variables: { input: { limit: 5 } },
+        data: {
+          myBooks: {
+            __typename: 'BookConnection',
+            edges: [
+              Object.assign(createBook, { __typename: 'Book' }),
+              ...edges
+            ],
+            pageInfo
+          }
         }
-      }
-    })
-  }
+      })
+    }
+  })
+
+  const editBook = useMutation()
 
   return (
-    <Mutation mutation={CREATEBOOK} update={updateCache}>
-      {(createBook, { data }) => (
-        <FullWidthForm
-          onSubmit={async e => {
-            e.preventDefault()
-            const {
-              data: {
-                createBook: { category }
-              }
-            } = await createBook({
-              variables: {
-                input: {
-                  title: bookTitle,
-                  author: bookAuthor,
-                  category: bookCategory,
-                  pages: parseInt(bookPages, 10),
-                  chapters: parseInt(bookChapters, 10)
-                }
-              }
-            })
+    <FullWidthForm
+      onSubmit={async e => {
+        e.preventDefault()
+        const {
+          data: {
+            createBook: { category }
+          }
+        } = await addBook({
+          variables: {
+            input: {
+              title: bookTitle,
+              author: bookAuthor,
+              category: bookCategory,
+              pages: parseInt(bookPages, 10),
+              chapters: parseInt(bookChapters, 10)
+            }
+          }
+        })
 
-            addToCategories({ variables: { cats: [category] } })
+        addToCategories({ variables: { cats: [category] } })
 
-            setTitle('')
-            setAuthor('')
-            setCategory('Novel')
-            setPages(1)
-            setChapters(1)
-            handleClose()
-          }}
-        >
-          <InputWrapper>
-            <LabelContainer>
-              <SmallLabel>Title</SmallLabel>
-              <Input
-                value={bookTitle}
-                onChange={handleTitle}
-                placeholder="Strides"
-              />
-            </LabelContainer>
+        setTitle('')
+        setAuthor('')
+        setCategory('Novel')
+        setPages(1)
+        setChapters(1)
+        handleClose()
+      }}
+    >
+      <InputWrapper>
+        <LabelContainer>
+          <SmallLabel>Title</SmallLabel>
+          <Input
+            value={bookTitle}
+            onChange={handleTitle}
+            placeholder="Strides"
+          />
+        </LabelContainer>
 
-            <LabelContainer>
-              <SmallLabel>Author</SmallLabel>
-              <Input
-                value={bookAuthor}
-                onChange={handleAuthor}
-                placeholder="Stephen Hill"
-              />
-            </LabelContainer>
-            <LabelContainer>
-              <SmallLabel>Category</SmallLabel>
-              <Select
-                name="category"
-                onChange={handleCategory}
-                value={bookCategory}
-              >
-                {categories.map(category => (
-                  <option key={category}>{category}</option>
-                ))}
-              </Select>
-            </LabelContainer>
-          </InputWrapper>
+        <LabelContainer>
+          <SmallLabel>Author</SmallLabel>
+          <Input
+            value={bookAuthor}
+            onChange={handleAuthor}
+            placeholder="Stephen Hill"
+          />
+        </LabelContainer>
+        <LabelContainer>
+          <SmallLabel>Category</SmallLabel>
+          <Select
+            name="category"
+            onChange={handleCategory}
+            value={bookCategory}
+          >
+            {categories.map(category => (
+              <option key={category}>{category}</option>
+            ))}
+          </Select>
+        </LabelContainer>
+      </InputWrapper>
 
-          <SmallInputWrapper>
-            <SmallLabelContainer>
-              <SmallLabel>Number of Pages</SmallLabel>
-              <NumberInput
-                min="1"
-                type="number"
-                value={bookPages}
-                onChange={handlePages}
-              />
-            </SmallLabelContainer>
+      <SmallInputWrapper>
+        <SmallLabelContainer>
+          <SmallLabel>Number of Pages</SmallLabel>
+          <NumberInput
+            min="1"
+            type="number"
+            value={bookPages}
+            onChange={handlePages}
+          />
+        </SmallLabelContainer>
 
-            <SmallLabelContainer>
-              <SmallLabel>Number of Chapters</SmallLabel>
-              <NumberInput
-                min="1"
-                type="number"
-                value={bookChapters}
-                onChange={handleChapters}
-              />
-            </SmallLabelContainer>
-            <SmallButton type="submit">Save</SmallButton>
-          </SmallInputWrapper>
-        </FullWidthForm>
-      )}
-    </Mutation>
+        <SmallLabelContainer>
+          <SmallLabel>Number of Chapters</SmallLabel>
+          <NumberInput
+            min="1"
+            type="number"
+            value={bookChapters}
+            onChange={handleChapters}
+          />
+        </SmallLabelContainer>
+        <SmallButton type="submit">Save</SmallButton>
+      </SmallInputWrapper>
+    </FullWidthForm>
   )
 }
 
